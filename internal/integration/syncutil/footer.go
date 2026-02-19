7256c3701470f480b82ff7b47f35df7b51457d92
Jason Abbott <jason@toba.app>
2026-02-19 14:19:02 -0700

    change sync note

--- internal/integration/syncutil/footer.go
+++ internal/integration/syncutil/footer.go
@@ -2,4 +2,4 @@ package syncutil
 
 // SyncFooter is the attribution footer appended to issues synced to external
 // services (GitHub, ClickUp) so viewers know the issue is managed externally.
-const SyncFooter = `\[⚠️ This issue is managed by [todo](https://github.com/toba/todo). Edits made here will be overwritten.\]`
+const SyncFooter = `👾 *This issue is managed by [todo](https://github.com/toba/todo). Edits made here will be overwritten.*`
